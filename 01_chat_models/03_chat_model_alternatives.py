d61181ee74f0380e1cb40c050836b9480169cd7e
mlbala <imbkrishnaa@gmail.com>
2024-11-16 19:16:57 +0530

    chat conversation with user

--- 01_chat_models/03_chat_model_alternatives.py
+++ 01_chat_models/03_chat_model_alternatives.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
 
 #Setup Environment Variable and messages
-load_dotenv()
+# load_dotenv()
 
 message = [
     SystemMessage(content="Solve the following math probles"),
@@ -23,12 +23,24 @@ model = ChatOpenAI(model = "gpt-4o-mini")
 result = model.invoke(message)
 print(f"Answer from Open AI : {result.content}")
 
-# --------------GOOGLE Chat Models Examples ----------------- #
+# # --------------GOOGLE Chat Models Examples ----------------- #
 
-# https://console.cloud.google.com/gen-app-builder/engines
-# https://ai.google.dev/gemini-api/docs/models/gemini
+# # https://console.cloud.google.com/gen-app-builder/engines
+# # https://ai.google.dev/gemini-api/docs/models/gemini
 
 model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash")
 
 result = model.invoke(message)
 print(f"Answer fro Google model : {result.content}")
+
+
+## ------------------------------- Anthropic model ------------------#
+
+
+# Create a Anthropic model
+# Anthropic models: https://docs.anthropic.com/en/docs/models-overview
+
+# model = ChatAnthropic(model = "claude-3-opus-20240229")
+
+# result = model.invoke(message)
+# print(f"Answer from Anthropic : {result.content}")
---
+++ 01_chat_models/04_chat_model_conversation_with_user.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+
+# load environment variables from .env
+load_dotenv()
+
+# Create a ChatOpenAI model
+
+model = ChatOpenAI(model = "gpt-4o-mini")
+
+#use a list to store messages
+chat_history = [] 
+
+# Set an initial system message 
+
+system_message = SystemMessage(content="You are a helpful AI assistant.")
+chat_history.append(system_message) #add system message
+
+# Chat look
+while True:
+    query = input("You : ")
+    if query.lower() == "exit":
+        break
+    chat_history.append(HumanMessage(content=query)) # add user query
+
+    # Get AI response using history
+    result = model.invoke(chat_history)
+    response = result.content
+    chat_history.append(AIMessage(content=response)) # AI message
+    
+    print(f"AI : {response}")
+
+print("----- Message History -----")
+# print(chat_history)
+
+for message in chat_history:
+    if isinstance(message, HumanMessage):
+        print(f"User: {message.content}")
+    elif isinstance(message, SystemMessage):
+        print(f"System: {message.content}")
+    elif isinstance(message, AIMessage):
+        print(f"AI: {message.content}")

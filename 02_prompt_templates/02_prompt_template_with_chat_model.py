7b54070daa88b3e5d24a2c2f21d9f4c4af7f001a
mlbala <imbkrishnaa@gmail.com>
2024-11-19 17:13:22 +0530

    prompt with multiple placeholder added

--- 02_prompt_templates/02_prompt_template_with_chat_model.py
+++ 02_prompt_templates/02_prompt_template_with_chat_model.py
@@ -16,3 +16,15 @@ prompt_template = ChatPromptTemplate.from_template(template)
 prompt = prompt_template.invoke({"topic":"cats"})
 result = model.invoke(prompt)
 print(result.content)
+
+# # PART 2: Prompt with Multiple Placeholders
+
+print("\n----- Prompt with Multiple Placeholders -----\n")
+template_multiple = """You are a helpful assistant.
+Human : Tell me a {adjective} short story about a {animal}.
+Assistant : """
+prompt_multiple = ChatPromptTemplate.from_template(template_multiple)
+prompt = prompt_multiple.invoke({"adjective": "funny", "animal": "panda"})
+
+result = model.invoke(prompt)
+print(result.content)

99c2859bdcb8670769f490a95e4a1e0e11b98e38
mlbala <imbkrishnaa@gmail.com>
2024-11-19 19:12:04 +0530

    chain under the hood with multiple topic dynamic count

--- 03_chains/02_chains_unde_the_hood.py
+++ 03_chains/02_chains_unde_the_hood.py
@@ -30,8 +30,46 @@ parse_output = RunnableLambda(lambda x : x.content)
 # Create the RunnableSequence (equivalent to the LCEL chain)
 chain = RunnableSequence(first=format_prompt, middle=[invoke_model], last=parse_output)
 
-# Run the chain
-response = chain.invoke({"topic":"lawyers", "joke_count": 5})
+# # Run the chain
+# response = chain.invoke({"topic":"lawyers", "joke_count": 5})
 
-#output
-print(response)
+# #output
+# print(response)
+
+
+# # For multiple topic
+# # Topics to generate joeks for
+# topics = ["lawyers","Software engineer","HR"]
+
+# # collect jokes for each topic
+# all_jokes  = {}
+# for topic in topics:
+#     response = chain.invoke({"topic": topic, "joke_count": 3})
+#     all_jokes[topic] = response
+
+# # Output the jokes
+
+# for topic, jokes in all_jokes.items():
+#     print(f"Jokes about {topic} : \n{jokes}\n")
+
+# For Dynamically specify the number of jokes for each topic
+
+# Topics and jokes count
+topics_and_counts = [
+    {"topic" : "lawyers","joke_count": 3},
+    {"topic" : "software engineer", "joke_count":2},
+    {"topic" : "HR", "joke_count": 3},
+]
+
+# Collect jokes  for each topic
+all_jokes = {}
+for entry in topics_and_counts:
+    topic = entry["topic"]
+    joke_count = entry["joke_count"]
+    response = chain.invoke({"topic": topic, "joke_count": joke_count})
+    all_jokes[topic] = response
+
+# Output the jokes
+
+for topic, jokes in all_jokes.items():
+    print(f"Jokes about {topic} :\n{jokes}")
